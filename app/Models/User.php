8ed0894ef3052ce4e3d62cf272a0540d8d7b86a5
Ricardo Martos <ricardo@webbook.dk>
2025-03-12 16:00:58 +0100

    add auth email rule

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
@@ -56,5 +57,10 @@ class User extends Authenticatable
             ->explode(' ')
             ->map(fn (string $name) => Str::of($name)->substr(0, 1))
             ->implode('');
+    }
+
+    static function isAdmin()
+    {
+        return Auth::user()->email = 'ricardomartoslecea@gmail.com';
     }
 }
